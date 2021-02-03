41d74f8b26d480cafcc402894724c7609c857d5b
Serj Sidelnikov <sv@spiks.ru>
2021-02-03 15:50:13 +0300

    WIP

---
+++ src/express/api.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const axios = require(`axios`);
+
+const TIMEOUT = 1000;
+const port = process.env.API_PORT || 3000;
+const defaultURL = `http://localhost:${port}/api/`;
+
+class API {
+  constructor(baseURL, timeout) {
+    this._http = axios.create({
+      baseURL,
+      timeout,
+    });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+    return response.data;
+  }
+}
