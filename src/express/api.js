76b5b2b08368663021aaed0cc680bf5da1f36541
Serj Sidelnikov <serj2107@gmail.com>
2021-02-04 20:00:27 +0300

    WIP

--- src/express/api.js
+++ src/express/api.js
@@ -18,4 +18,34 @@ class API {
     const response = await this._http.request({url, ...options});
     return response.data;
   }
+
+  getOffers() {
+    return this._load(`/offers`);
+  }
+
+  getOffer(id) {
+    return this._load(`/offers/${id}`);
+  }
+
+  createOffer(data) {
+    return this._load(`/offers`, {
+      method: `POST`,
+      data,
+    });
+  }
+
+  getCategories() {
+    return this._load(`/categories`);
+  }
+
+  search(query) {
+    return this._load(`/search`, {params: {query}});
+  }
 }
+
+const defaultAPI = new API(defaultURL, TIMEOUT);
+
+module.exports = {
+  API,
+  getAPI: () => defaultAPI,
+};
